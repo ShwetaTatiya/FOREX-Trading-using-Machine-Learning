dbff798125c3e6e0d703792a1c2b29b02b46a962
Shweta Tatiya <tatiya.s@husky.neu.edu>
2019-08-02 18:59:49 -0400

    added

---
+++ requirements.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Copyright (c) Tutorialspoint.com
+# Script follows here:
+
+pip install numpy
+pip install pandas
+pip install missingno
+pip install matplotlib
+pip install seaborn
+pip install datetime 
+pip install missingno
+pip install pandas_profiling
+pip install scipy
+pip install scipy
+pip install math 
+pip install pandas_profiling
+pip install sklearn
+pip install tensorflow as tf
+pip install plotly
+pip install cufflinks as cf
